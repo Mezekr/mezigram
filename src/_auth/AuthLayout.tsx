e12873e0639ca7695fa94b47dc4db42cbb65f635
Mezekr W <mezi.edu@tuta.io>
2024-07-28 10:35:09 +0200

    create AuthLayout pages

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
-	return <div>AuthLayout</div>;
+	const isAuthenticated = false;
+
+	return (
+		<>
+			{isAuthenticated ? (
+				<Navigate to={'/'} />
+			) : (
+				<>
+					<section
+						className="flex flex-1 justify-center items-center
+									flex-column py-10"
+					>
+						<Outlet />
+					</section>
+					<img
+						src="/assets/images/side-img.svg"
+						alt="App logo image"
+						className=" hidden xl:block h-screen w-1/2 object-cover  bg-no-repeat"
+					/>
+				</>
+			)}
+		</>
+	);
 };
 
 export default AuthLayout;
