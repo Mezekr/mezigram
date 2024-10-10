ac410d758394567f38927bd71684234d217ad96f
Mezekr W <mezi.edu@tuta.io>
2024-10-10 19:26:14 +0200

    add Initial User and Initial State for AuthContext

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+export const INITIAL_USER = {
+	id: '',
+	name: '',
+	username: '',
+	email: '',
+	imageUrl: '',
+	bio: '',
+};
+
+export const INITIAL_STATE = {
+	user: INITIAL_USER,
+	isLoading: false,
+	isAuthenticated: false,
+	setUser: () => {},
+	setIsAuthenticated: () => {},
+	checkAuthUser: async () => false as boolean,
+};
+
+function AuthContext() {
+	return <div>AuthContext</div>;
+}
+
+export default AuthContext;
